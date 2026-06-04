3211ff349d0a19438cc6025860fd79049b93a4e1
Mateusz Stasiak <mateusz.stasiak16@gmail.com>
2026-06-04 18:14:48 +0200

    Laboratory 1

--- controllers/feedback_linearization_controller.py
+++ controllers/feedback_linearization_controller.py
@@ -6,10 +6,26 @@ from .controller import Controller
 class FeedbackLinearizationController(Controller):
     def __init__(self, Tp):
         self.model = ManiuplatorModel(Tp)
+        self.Kp = 55
+        self.Kd = 10
 
     def calculate_control(self, x, q_r, q_r_dot, q_r_ddot):
         """
         Please implement the feedback linearization using self.model (which you have to implement also),
         robot state x and desired control v.
         """
-        return NotImplementedError()
+        q = x[:2]
+        q_dot = x[2:]
+
+        M = self.model.M(x)
+        C = self.model.C(x)
+
+        # 2.
+        # v = q_r_ddot
+        # u = M @ v + C @ q_dot
+
+        # 8.
+        v = q_r_ddot + self.Kd * (q_r_dot - q_dot) + self.Kp * (q_r - q)
+        u = M @ v + C @ q_dot
+
+        return u
--- models/manipulator_model.py
+++ models/manipulator_model.py
@@ -5,16 +5,32 @@ class ManiuplatorModel:
     def __init__(self, Tp):
         self.Tp = Tp
         self.l1 = 0.5
-        self.r1 = 0.01
-        self.m1 = 1.
-        self.l2 = 0.5
-        self.r2 = 0.01
-        self.m2 = 1.
+        self.r1 = 0.04
+        self.m1 = 3.0
+
+        self.l2 = 0.4
+        self.r2 = 0.04
+        self.m2 = 2.4
+
+        self.m3 = 0.7
+        self.r3 = 0.05
+
         self.I_1 = 1 / 12 * self.m1 * (3 * self.r1 ** 2 + self.l1 ** 2)
         self.I_2 = 1 / 12 * self.m2 * (3 * self.r2 ** 2 + self.l2 ** 2)
-        self.m3 = 0.0
-        self.r3 = 0.01
         self.I_3 = 2. / 5 * self.m3 * self.r3 ** 2
+
+        self.d1 = self.l1 / 2
+        self.d2 = self.l2 / 2
+
+        # 1.
+        self.alpha = self.m1 * self.d1 ** 2 + self.I_1 + self.m2 * (self.l1 ** 2 + self.d2 ** 2) + self.I_2
+        self.beta = self.m2 * self.l1 * self.d2
+        self.gamma = self.m2 * self.d2 ** 2 + self.I_2
+
+        # 4.
+        self.alpha += self.m3 * (self.l1 ** 2 + self.l2 ** 2) + self.I_3
+        self.beta += self.m3 * self.l1 * self.l2
+        self.gamma += self.m3 * self.l2 ** 2 + self.I_3
 
     def M(self, x):
         """
@@ -22,7 +38,14 @@ class ManiuplatorModel:
         (2DoF planar manipulator with the object at the tip)
         """
         q1, q2, q1_dot, q2_dot = x
-        return NotImplementedError()
+
+        m_11 = self.alpha + 2 * self.beta * np.cos(q2)
+        m_12 = self.gamma + self.beta * np.cos(q2)
+        m_21 = m_12
+        m_22 = self.gamma
+
+        matrix = np.array([[m_11, m_12], [m_21, m_22]])
+        return matrix
 
     def C(self, x):
         """
@@ -30,4 +53,11 @@ class ManiuplatorModel:
         in the exercise (2DoF planar manipulator with the object at the tip)
         """
         q1, q2, q1_dot, q2_dot = x
-        return NotImplementedError()
+
+        c_11 = -self.beta * np.sin(q2) * q2_dot
+        c_12 = -self.beta * np.sin(q2) * (q1_dot + q2_dot)
+        c_21 = self.beta * np.sin(q2) * q1_dot
+        c_22 = 0
+
+        matrix = np.array([[c_11, c_12], [c_21, c_22]])
+        return matrix
